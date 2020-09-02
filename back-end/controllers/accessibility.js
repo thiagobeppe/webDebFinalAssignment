212e959f683d3d179328816ac2fc45ea3f50cf66
romulofff <romulo_freire_filho@hotmail.com>
2020-09-02 18:59:20 -0300

    Opção do usuário salva no local storage

--- back-end/controllers/accessibility.js
+++ back-end/controllers/accessibility.js
@@ -2,7 +2,9 @@ function changeFontSize(value) {
     txt = document.body;
     css_font_size = window.getComputedStyle(txt, null).getPropertyValue('font-size');
     font_size = parseFloat(css_font_size);
+    newSize = (font_size + value) + 'px';
     txt.style.fontSize = (font_size + value) + 'px';
+    localStorage.setItem('font-size', newSize)
 }
 
 function swapStyleSheet(sheet) {
@@ -18,18 +20,32 @@ function baseName(str)
     return base;
 }
 
+window.onload = initTheme()
+function initTheme() {
+    if (!localStorage.getItem('stylesheet')) {
+        console.log("First access!")
+    }
+    else {
+        var theme = localStorage.getItem('stylesheet')
+        swapStyleSheet(`assets/css/${theme}.css`)
+    }
+}
+
 function initiate() {
     var styleNow = document.getElementById("contrast");
     var bname = baseName(styleNow.href)
-
+        
     if (bname.includes("contrast")) {
+        console.log(bname)
         bname = bname.split('_')[0]
+        var prefered = `${bname}`
+        console.log(bname)
         swapStyleSheet(`assets/css/${bname}.css`);
     } else {      
+        var prefered = `${bname}_contrast`
         swapStyleSheet(`assets/css/${bname}_contrast.css`);        
     }
     
     var styleNow = document.getElementById("contrast");
-    
-    window.localStorage.stylesheet = styleNow;
+    localStorage.setItem('stylesheet', prefered);
 }
