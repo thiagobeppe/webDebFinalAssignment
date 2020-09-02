7eef82389070330d91eff77f374e4243462a29d7
airtonfilho04 <airtonfilhocontato@outlook.com>
2020-09-02 16:55:08 -0300

    Testing back end

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,18 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const mongoose = require('mongoose');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const app = express();
+
+mongoose.connect('mongodb+srv://eduardo_bz:adocao123@adocao.qzwle.mongodb.net/Adocao?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+
+app.post('/users', function (req, res) {
+  console.log(req.body);
+  return res.json({ message: 'Funfou!' });
 });
 
 app.listen(3000, function () {
