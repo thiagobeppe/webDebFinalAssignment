a3224f54d9a7df6e5b2c172bc498cae817873c22
Guilherme <guilherme.araujo@itegra.com.br>
2020-09-02 22:48:51 -0300

    listando pets do bd na index

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -11,6 +12,7 @@ mongoose.connect(
   }
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
