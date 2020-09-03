2e745a94e800f3a9df65d1e6a69a9f64928f551c
airtonfilho04 <airtonfilhocontato@outlook.com>
2020-09-03 00:14:09 -0300

    Rotas de pet e users, cadastro de pet e users, lista de users

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
 const routes = require("./routes");
 const app = express();
 
@@ -11,8 +12,35 @@ mongoose.connect(
   }
 );
 
-app.use(express.json());
+// Confirma ligação na consola
+mongoose.connection.on('connected', function () {
+  console.log('Connected to Database');
+});
+
+// Mensagem de Erro
+mongoose.connection.on('error', (err) => {
+  console.log('Database error'+err);
+});
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes);
+app.set('views', './src/views');
+
+app.get('/users/signup', function (req, res) {
+  res.render('signup');
+});
+
+app.get('/pets/create', function (req, res) {
+  res.render('newPet');
+});
+
+app.get('/dashboard-admin/users', function (req, res) {
+  res.render('dashboard_admin_cruduser');
+});
 
 app.listen(3000, function () {
   console.log("Example app listening on port 3000!");
