b737ce74892f8d4b79021a1fbccea89d8f1fe363
thiagobeppe <thiagoabeppe@gmail.com>
2020-09-03 12:50:46 -0300

    adding admin

---
+++ back-end/src/controllers/AdminController.js
@@ -0,0 +1,31 @@
+const Admin = require("../models/Admin");
+const { request } = require("express");
+const { create } = require("../models/Admin");
+
+
+module.exports = {
+    async login(request, response) {
+        let email = request.body.email;
+        let pwd = request.body.password;
+        var usr = await Admin.findOne({'email': email})
+        if (usr != null) {
+          if (pwd == usr.password){
+            response.render('dashboard_admin',data=usr)
+          }
+          else {
+            response.status(500).render("signin_admin")
+          }
+        }
+        else   {response.status(500).render("signin_admin")}
+      },
+      async create(request, response){
+        let email = request.body.email;
+        let pwd = request.body.password;
+
+        let data = {
+            email, 
+            password: pwd
+        }
+        var admin = await Admin.create(data)
+      }
+}
--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const PetController = require("./controllers/PetController");
 const UserController = require("./controllers/UserController");
+const AdminController = require("./controllers/AdminController");
 
 const routes = Router();
 
@@ -11,16 +12,16 @@ const routes = Router();
 routes.get("/", PetController.listpets)
 
 routes.post("/pets/newpet", PetController.addpet);
-
 routes.get("/pets", PetController.index);
 
+routes.get("/users", UserController.index);
+routes.post("/users/login", UserController.login);
 routes.post('/users/newuser', UserController.adduser);
 
 routes.get("/dashboard-admin/users", UserController.listusers);
+routes.post("/admin/login", AdminController.login)
+routes.post("/admin/create", AdminController.create)
 
-routes.get("/users", UserController.index);
-
-routes.post("/login", UserController.login);
 routes.get("/signin", UserController.signInUser);
 
 module.exports = routes;
